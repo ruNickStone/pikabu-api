1968987dd712f2b2e44ec62ce6b827fc7b87e4fb
melanholy <irrygan@gmail.com>
2015-05-13 20:41:49 +0500

    much properties, so pylint, very other

--- api.py
+++ api.py
@@ -8,7 +8,25 @@ import requests
 import datetime
 from bs4 import BeautifulSoup
 
-class API:
+def parse_date(date):
+    if u'сегодня' in date:
+        return calendar.timegm(datetime.datetime.today().utctimetuple())
+    part_str = date.split()
+    year, month, day = 1, 1, 1
+    for val, index in [(int(x), i) for i, x in enumerate(part_str) if x.isdigit()]:
+        if part_str[index+1].startswith(u'ле') or part_str[index+1].startswith(u'го'):
+            year += val
+        elif part_str[index+1].startswith(u'ме'):
+            month += val
+        elif part_str[index+1].startswith(u'не'):
+            day += 7*val
+        elif part_str[index+1].startswith(u'дн') or part_str[index+1].startswith(u'де'):
+            day += val
+    diff = datetime.datetime.today()-datetime.datetime(year, month, day)
+    reg_date = datetime.datetime(1, 1, 1)+datetime.timedelta(diff.days)
+    return calendar.timegm(reg_date.utctimetuple())
+
+class API(object):
 
     site_url = 'http://pikabu.ru/'
 
@@ -31,10 +49,12 @@ class PikaService(object):
         self.settings = settings
         self.xcsrf_token = None
 
-    def request(self, url, data=None, method='GET', referer=API.site_url, custom_headers=None, need_auth=True):
+    def request(self, url, data=None, method='GET', referer=API.site_url,
+                custom_headers=None, need_auth=True):
         self.xcsrf_token = requests.get(API.site_url).cookies['PHPSESS']
         post_headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36',
             'Referer': referer,
             'Host': 'pikabu.ru',
             'Origin': 'http://pikabu.ru',
@@ -52,53 +72,53 @@ class PikaService(object):
                 'remember': 0
             }
 
-            r = self.pikabu_sess.post(
+            response = self.pikabu_sess.post(
                 API.site_url + 'ajax/ajax_login.php',
                 data=login_data,
                 headers=post_headers,
                 cookies={'PHPSESS': self.xcsrf_token}
             )
-            r = json.loads(r.text)
-            if int(r['logined']) == 0:
+
+            response = json.loads(response.text)
+            if int(response['logined']) == 0:
                 raise ValueError('Неверно указан логин или пароль')
-            if int(r['logined']) == -1:
+            if int(response['logined']) == -1:
                 raise ValueError(['error'])
             self.is_logged = True
 
-        req = requests.Request(method, API.site_url + url,
-            data = data,
-            headers = custom_headers if(custom_headers is not None) else post_headers,
-            cookies = {'PHPSESS': self.xcsrf_token}
+        req = requests.Request(
+            method,
+            API.site_url + url,
+            data=data,
+            headers=custom_headers if(custom_headers is not None) else post_headers,
+            cookies={'PHPSESS': self.xcsrf_token}
         )
+
         prepped = req.prepare()
         resp = self.pikabu_sess.send(prepped)
         resp.raise_for_status()
 
         return resp.text
 
-
 class PikabuPosts(PikaService):
 
     def rate(self, action, post_id):
-        if post_id:
-            if action == '+' or action == 1:
-                act = '+'
-            elif action == '-' or action == 0:
-                act = '-'
-            else:
-                return False
-
-            rate_data = {
-                'i': post_id,
-                'type': act
-            }
-            
-            page = self.request('ajax/dig.php', method='POST', data=rate_data)
-            return page
-        else:
+        if not post_id:
             raise ValueError('Invalid post ID')
-        
+        if action == '+' or action == 1:
+            act = '+'
+        elif action == '-' or action == 0:
+            act = '-'
+        else:
+            return False
 
+        rate_data = {
+            'i': post_id,
+            'type': act
+        }
+        
+        page = self.request('ajax/dig.php', method='POST', data=rate_data)
+        return page
 
 class PikabuComments(PikaService):
 
@@ -111,9 +131,16 @@ class PikabuComments(PikaService):
             for comment in comments:
                 comment_date = calendar.timegm(datetime.datetime.strptime(
                     comment['date'], '%Y-%m-%d %H:%M').utctimetuple())  # date to timestamp
-                comment_list.append(ObjectComments(int(comment['id']), int(comment['rating']),
-                                                   comment['nick'], int(comment['answer']),
-                                                   comment_date, comment.text))
+                comment_list.append(
+                    ObjectComments(
+                        int(comment['id']),
+                        int(comment['rating']),
+                        comment['nick'],
+                        int(comment['answer']),
+                        comment_date, comment.text
+                    )
+                )
+
             return comment_list
         else:
             raise ValueError('Invalid post ID')
@@ -128,7 +155,9 @@ class PikabuComments(PikaService):
                 'include': 0,
                 'comment_images': ''
             }
-            referer = requests.head('%s/story/_%i' % (API.site_url, post_id), allow_redirects=False).headers['location']
+
+            path = '%s/story/_%i' % (API.site_url, post_id)
+            referer = requests.head(path, allow_redirects=False).headers['location']
             page = self.request('ajax.php', comment_data, method='POST', referer=referer)
             response = json.loads(page)
             return True if(response['type'] == 'done') else response['text']
@@ -143,13 +172,14 @@ class PikabuComments(PikaService):
                 act = 0
             else:
                 raise ValueError('Invalid action')
-
-            referer = requests.head('%s/story/_%i' % (API.site_url, post_id), allow_redirects=False).headers['location']
+            path = '%s/story/_%i' % (API.site_url, post_id)
+            referer = requests.head(path, allow_redirects=False).headers['location']
             custom_headers = {
                 'Accept': '*/*',
                 'Host': 'pikabu.ru',
                 'Referer': referer,
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 '
+                              '(KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36'
             }
 
             rate_data = {
@@ -158,8 +188,14 @@ class PikabuComments(PikaService):
                 'story': post_id,
                 'dir': act
             }
-            
-            page = self.request('dig.php', data=rate_data, custom_headers=custom_headers, referer=referer)
+
+            page = self.request(
+                'dig.php',
+                data=rate_data,
+                custom_headers=custom_headers,
+                referer=referer
+            )
+
             return page
         else:
             raise ValueError('Invalid post or comment ID')
@@ -173,60 +209,55 @@ class PikabuUserInfo(PikaService):
     def get(self, login):
         page = self.request('profile/%s' % login, need_auth=False)
         self.html = BeautifulSoup(page).find('div', 'profile_wrap')
-        self.info = self.html.find('div', {'style':'padding-top: 0px; line-height: 15px;'}).text.split('\n')
+        info = self.html.find('div', {'style':'padding-top: 0px; line-height: 15px;'})
+        self.info = info.text.split('\n')
+        return ObjectUserInfo(
+            login,
+            self.dor,
+            self.rating,
+            self.avatar,
+            self.comments,
+            self.news,
+            self.actions,
+            self.awards
+        )
 
-        return ObjectUserInfo(login, self.get_dor(),
-            self.get_rating(), self.get_avatar(), self.get_comments(),
-            self.get_news(), self.get_actions(), self.get_awards())
-
-    def get_dor(self):
+    @property
+    def dor(self):
         """Возвращает дату регистрации пользователя"""
-        return self.parse_date(self.info[3])
+        return parse_date(self.info[3])
 
-    def get_rating(self):
+    @property
+    def rating(self):
         """Возвращает рейтинг юзера"""
         return int(self.info[4].split()[1])
-
-    def get_avatar(self):
+    @property
+    def avatar(self):
         """Возвращает аватар юзера"""
         return self.html.find('img')['src']
 
-    def get_comments(self):
+    @property
+    def comments(self):
         """Возвращает количество комментариев юзера"""
         return int(self.info[5].split()[1])
 
-    def get_news(self):
+    @property
+    def news(self):
         """Возвращает массив с количеством новостей"""
-        return list(map(lambda x: int(x), re.findall(r'\d+', self.info[6])))
+        return list(map(int, re.findall(r'\d+', self.info[6])))
 
-    def get_actions(self):
+    @property
+    def actions(self):
         """Возвращает массив с количество + и - юзера"""
         return [int(self.info[10].split()[0]), int(self.info[12].split()[0])]
 
-    def get_awards(self):
+    @property
+    def awards(self):
         """Возвращает список наград пользователя"""
         _ = self.html.find('div', 'awards_wrap').findAll('img')
-        return list(map(lambda x: (x['title'], x['src']), _))
+        return list([(x['title'], x['src']) for x in _])
 
-    def parse_date(self, date):
-        if u'сегодня' in date:
-            return calendar.timegm(datetime.datetime.today().utctimetuple())
-        part_str = date.split()
-        year, month, day = 1, 1, 1
-        for val, index in [(int(x), i) for i, x in enumerate(part_str) if x.isdigit()]:
-            if part_str[index+1].startswith(u'ле') or part_str[index+1].startswith(u'го'):
-                year += val
-            elif part_str[index+1].startswith(u'ме'):
-                month += val
-            elif part_str[index+1].startswith(u'не'):
-                day += 7*val
-            elif part_str[index+1].startswith(u'дн') or part_str[index+1].startswith(u'де'):
-                day += val
-        diff = datetime.datetime.today()-datetime.datetime(year, month, day)
-        reg_date = datetime.datetime(1, 1, 1)+datetime.timedelta(diff.days)
-        return calendar.timegm(reg_date.utctimetuple())
-
-class PikabuProfile(PikabuUserInfo):
+class PikabuProfile(PikaService):
     """Профиль авторизованного пользователя"""
     def __init__(self, **settings):
         self.html = None
@@ -238,13 +269,23 @@ class PikabuProfile(PikabuUserInfo):
         self.html = BeautifulSoup(page)
         profile = PikabuUserInfo()
         profile = profile.get(self.settings['login'])
-        return ObjectUserInfo(self.settings['login'], profile.dor,
-            profile.rating, profile.comments, profile.avatar,
-            profile.news, profile.actions, profile.awards, self.get_followers())
+        return ObjectUserInfo(
+            self.settings['login'],
+            profile.dor,
+            profile.rating,
+            profile.comments,
+            profile.avatar,
+            profile.news,
+            profile.actions,
+            profile.awards,
+            self.followers
+        )
 
-    def get_followers(self):
+    @property
+    def followers(self):
         """Возвращает количество подписчиков"""
-        return int(''.join(s for s in self.html.find('ul', 'b-user-menu-list').findAll('li')[1].text if s.isdigit()))
+        user_list = self.html.find('ul', 'b-user-menu-list').findAll('li')[1].text
+        return int(''.join(s for s in user_list if s.isdigit()))
 
 class PikabuTopTags(PikaService):
 
@@ -252,15 +293,19 @@ class PikabuTopTags(PikaService):
         if limit > 0:
             page = self.request('html.php?id=faq')
             tags = BeautifulSoup(page)
-            tag_list = list(zip(map(lambda x: x.text, tags.findAll('span', 'tag no_ch')), map(lambda y: int(y.text), tags.findAll('span', 'tag_count'))))
+            tag_list = list(zip(
+                [x.text for x in tags.findAll('span', 'tag no_ch')],
+                [int(y.text) for y in tags.findAll('span', 'tag_count')]
+            ))
             return tag_list[:limit]
         else:
             return False
 
 
-class ObjectUserInfo():
+class ObjectUserInfo(object):
 
-    def __init__(self, login, dor, rating, avatar, comments, news, actions, awards, followers=None):
+    def __init__(self, login, dor, rating, avatar, comments,
+                 news, actions, awards, followers=None):
         self.login = login
         self.dor = dor
         self.rating = rating
@@ -269,10 +314,11 @@ class ObjectUserInfo():
         self.news = news
         self.actions = actions
         self.awards = awards
-        if followers is not None:
-            self.followers = followers
+        if followers is None:
+            followers = []
+        self.followers = followers
 
-class ObjectComments():
+class ObjectComments(object):
 
     def __init__(self, id, rating, nick, answer, date, text):
         self.id = id
